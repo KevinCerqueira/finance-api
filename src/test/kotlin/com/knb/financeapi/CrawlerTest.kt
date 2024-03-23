1dbb2e63ab0207abce33e51af06af0e462d7c0b0
Kevin Cerqueira <38994372+KevinCerqueira@users.noreply.github.com>
2024-03-22 22:54:43 -0300

    tests: tests for the Crawler class

---
+++ src/test/kotlin/com/knb/financeapi/CrawlerTest.kt
@@ -0,0 +1,65 @@
+package com.knb.financeapi
+
+import org.jsoup.nodes.Document
+import org.jsoup.Jsoup
+import org.junit.jupiter.api.Assertions.*
+import org.junit.jupiter.api.Test
+import org.mockito.Mockito
+
+class CrawlerTest {
+
+    @Test
+    fun testGetStockSuccess() {
+        val html = "<html><body>" +
+                "<div class='zzDege'>Test Stock</div>" +
+                "<div class='YMlKec fxKbKc'>R$1,234.56</div>" +
+                "<div class='bLLb2d'>Informações sobre a ação</div>" +
+                "</body></html>"
+        val doc: Document = Jsoup.parse(html)
+
+        val crawler = Mockito.spy(Crawler::class.java)
+        Mockito.doReturn(doc).`when`(crawler).request(Mockito.anyString())
+
+        val stock = crawler.getStock("TEST")
+
+        assertNotNull(stock)
+        assertEquals("Test Stock", stock?.name)
+        assertEquals(1234.56, stock?.price)
+        assertEquals("Informações sobre a ação", stock?.about)
+    }
+
+    @Test
+    fun testExceptionHandling() {
+        val crawler = Mockito.spy(Crawler::class.java)
+        Mockito.doReturn(null).`when`(crawler).request(Mockito.anyString())
+
+        val stock = crawler.getStock("INVALID")
+        assertNull(stock)
+    }
+
+    @Test
+    fun testMissingValues() {
+        val html = "<html><body></body></html>"
+        val doc: Document = Jsoup.parse(html)
+
+        val crawler = Mockito.spy(Crawler::class.java)
+        Mockito.doReturn(doc).`when`(crawler).request(Mockito.anyString())
+
+        val stock = crawler.getStock("MISSING")
+        assertNull(stock?.name)
+        assertNull(stock?.price)
+        assertNull(stock?.about)
+    }
+
+    @Test
+    fun testValueFormatting() {
+        val html = "<html><body><div class='YMlKec fxKbKc'>R$1,234.56</div></body></html>"
+        val doc: Document = Jsoup.parse(html)
+
+        val crawler = Mockito.spy(Crawler::class.java)
+        Mockito.doReturn(doc).`when`(crawler).request(Mockito.anyString())
+
+        val value = crawler.getValue(doc)
+        assertEquals(1234.56, value)
+    }
+}
